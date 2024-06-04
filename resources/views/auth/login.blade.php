d598043628599b4f152cd6b94be09a5f8b4e49f8
VladPtashnyk06 <vladptashnyk06@gmail.com>
2024-06-04 16:38:15 +0300

    DEV-4: Edited files

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,19 +32,19 @@
                     <div class="block mt-4">
                         <label for="remember_me" class="inline-flex items-center">
                             <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" name="remember">
-                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Запам`ятати мене') }}</span>
                         </label>
                     </div>
 
                     <div class="flex items-center justify-between mt-4">
                         @if (Route::has('password.request'))
                             <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
+                                {{ __('Забули свій пароль ?') }}
                             </a>
                         @endif
 
                         <x-primary-button class="ml-3">
-                            {{ __('Log in') }}
+                            {{ __('Увійти') }}
                         </x-primary-button>
                     </div>
                 </form>
